6bf27d4eafff183898b0eecc31bac0e22bf1116d
15'Slamyxx <140884344+15Slamyxx@users.noreply.github.com>
2024-05-09 23:29:32 +0200

    Ajoutez des fichiers projet.

---
+++ Example-Plugin/Config.cs
@@ -0,0 +1,10 @@
+﻿using Exiled.API.Interfaces;
+
+namespace Example_Plugin
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; }
+    }
+}
---
+++ Example-Plugin/EventHandlers.cs
@@ -0,0 +1,25 @@
+﻿using Exiled.API.Features;
+namespace Example_Plugin
+{
+    public class EventHandlers : Plugin<Config>
+    {
+
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+            Exiled.Events.Handlers.Server.RoundStarted += CassieMessage;
+        }
+
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+            Exiled.Events.Handlers.Server.RoundStarted -= CassieMessage;
+        }
+
+        private void CassieMessage()
+        {
+            Cassie.MessageTranslated("", "", isHeld: false, isNoisy: false, isSubtitles: true); //Pour faire un message cassie debut de game
+            Map.TurnOffAllLights(4); //Pour éteindre les lumiére pendant 4Seconde
+        }
+    }
+}
